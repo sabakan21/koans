e60ca5581f802325872245bb2dc254dddc508f7a
FUJII Akira <fujii@sein.mse.kyutech.ac.jp>
2015-06-18 15:55:56 +0900

    fix classes, open_classes and dice_project

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -1,10 +1,24 @@
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # Implement a DiceSet Class here:
-#
-# class DiceSet
-#   code ...
-# end
+
+class DiceSet
+
+  def initialize
+    @values = Array.new
+  end
+
+  def roll(roll_time)
+    @values = Array.new
+    roll_time.times do
+      @values << rand(6) + 1
+    end
+  end
+
+  def values
+    @values
+  end
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
